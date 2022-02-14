7968a879628bd0e03a6db5fb9d2dd56b8356f1c4
G.Cerro <giorgiocerro96@gmail.com>
2022-02-14 11:48:29 +0000

    removed info printing

--- hyperTest.py
+++ hyperTest.py
@@ -263,7 +263,7 @@ class HyperEmbedding:
                 self.embeddings[indices_to_update] = self.manifold.metric.exp(
                         -self.lr * grad_temp[indices_to_update],
                         self.embeddings[indices_to_update])
-                logging.info(f'epoch:{epoch},step:{step},loss:{loss}')
+                #logging.info(f'epoch:{epoch},step:{step},loss:{loss}')
                
                 if images:
                     if step%2 ==0:
@@ -276,7 +276,7 @@ class HyperEmbedding:
     def save_img(self,step):
         fig,ax = plt.subplots(figsize=(20,10))
         degrees = self.edge_pivot.out.apply(lambda x: len(x)*10).values
-        '''
+        
         ax.scatter(self.embeddings[~self.graph.final.data,0],
             self.embeddings[~self.graph.final.data,1],
             10*np.log(degrees[~self.graph.final.data]),
@@ -285,6 +285,7 @@ class HyperEmbedding:
             self.embeddings[self.graph.final.data,1],
             10*np.log(degrees[self.graph.final.data]),
             alpha=1,color='#FF1AE3')
+        
         '''
         ax.scatter(self.embeddings[:,0],self.embeddings[:,1],
             alpha=1,color='#1AF8FF',s=50)
@@ -292,6 +293,7 @@ class HyperEmbedding:
             x0,y0 = self.embeddings[edge[0]]
             x1,y1 = self.embeddings[edge[1]]
             ax.plot([x0,x1],[y0,y1],alpha=0.3,color='#FF1AE3')
+        '''
         disk=Circle((0,0),1,color='white',fill=False)
         ax.add_patch(disk)
         ax.set_xlim(-1,1)
