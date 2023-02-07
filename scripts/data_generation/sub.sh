3d0fbf66bd4b7b84e922edeb2ecade1c8963182f
G.Cerro <giorgiocerro96@gmail.com>
2023-02-07 10:04:38 +0000

    more cleaning

--- scripts/data_generation/sub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH --ntasks-per-node=25     # Tasks per node
-#SBATCH --nodes=1                # Number of nodes requested
-#SBATCH --time=00:10:00          # walltime
-
-source activate pyg
-mpiexec -n 25 python3 make_dataset.py ../data/signal/hz_train.lhe.gz pythia-settings.cmnd ../data/signal/hz_train.hdf5
-source deactivate
